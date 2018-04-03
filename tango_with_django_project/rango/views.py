132725860ae852f2561c14e6151fb87b3ecae772
carry <290651463@qq.com>
2018-04-03 16:23:29 +0800

    add Exercises

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("rango says hey there partner!")
+    return HttpResponse("rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+def about(request):
+    return HttpResponse("rango says here is the about page. <a href='/rango/'>Index</a>")
