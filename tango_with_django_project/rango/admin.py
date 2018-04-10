033061df10a28c95e26d9d2bf1f66cb4def1c402
carry <290651463@qq.com>
2018-04-10 18:52:14 +0800

    Cookies and Sessions

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
-# from rango.models import UserProfile
+from rango.models import UserProfile
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
@@ -11,4 +11,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
-# admin.site.register(UserProfile)
+admin.site.register(UserProfile)
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -37,4 +38,16 @@ class Page(models.Model):
             cleaned_data['url'] = url
             return cleaned_data
 
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def __unicode__(self):
+        return self.user.username
 
